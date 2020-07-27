ada040b1a0ec255be3bec19b54171daf7c45f6ac
vagrant <vagrant@localhost.localdomain>
2020-07-27 13:00:25 +0000

    add_top_favoriteshop

--- app/controllers/admins/homes_controller.rb
+++ app/controllers/admins/homes_controller.rb
@@ -2,6 +2,8 @@ class Admins::HomesController < ApplicationController
 	before_action :authenticate_admin!
 	def top
 		@shops = Shop.all
-		@shop_favorite = @shops.where(is_favorite: true)
+		@shop_favorite = @shops.where(is_favorite: true).where(is_active: true)
+		@shop_favorite_odd = @shop_favorite.each_slice(2).map(&:first)
+		@shop_favorite_even = @shop_favorite.each_slice(2).map(&:last)
 	end
 end
--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -2,7 +2,9 @@ class ShopsController < ApplicationController
 
 	def top
 		@shops = Shop.all
-		@shop_favorite = @shops.where(is_favorite: true)
+		@shop_favorite = @shops.where(is_favorite: true).where(is_active: true)
+		@shop_favorite_odd = @shop_favorite.each_slice(2).map(&:first)
+		@shop_favorite_even = @shop_favorite.each_slice(2).map(&:last)
 	end
 
 	def search
